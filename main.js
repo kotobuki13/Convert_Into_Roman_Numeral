69f1805f7d166fea0d7c19f4fe7d91373b2f1866
kotobuki13 <kotobukigit@yahoo.co.jp>
2020-01-01 11:51:28 -0800

    make basic program

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
-let value = 1; // get value from user
-const letters = new Array(5); // m, d, c, l, x
+const value = 130; // get value from user
+const result = []
+const letters = ["m", "d", "c", "l", "x", "v", "i"];
 
 function devideNumber(value) {
+
+  let numbers = new Array(7); // m, d, c, l, x, v, remain
+
   if (value / 1000 >= 1) {
-    letters[0] = value / 1000;
-    value -= letters[0] * 1000;
+    numbers[0] = value / 1000;
+    value -= numbers[0] * 1000;
   }
   if (value / 500 >= 1) {
-    letters[1] = value / 500;
-    value -= letters[1] * 500;
+    numbers[1] += 1;
   }
   if (value / 100 >= 1) {
-    letters[2] = value / 100;
-    value -= letters[2] * 100;
+    numbers[2] = value / 100;
+    value -= numbers[2] * 100;
   }
   if (value / 50 >= 1) {
-    letters[3] = value / 50;
-    value -= letters[3] * 50;
+    numbers[3] += 1;
   }
   if (value / 10 >= 1) {
-    letters[4] = value / 10;
-    value -= letters[4] * 10;
+    numbers[4] = value / 10;
+    value -= numbers[4] * 10;
+  }
+  if (value / 5 >= 1) {
+    numbers[5] += 1;
   }
   if (value > 0) {
-    getU8Code(value);
+    numbers[6] = value;
   }
+  writeRomanNum(numbers);
+  return;
+}
+
+function writeRomanNum(numbers) {
+  for (i = 0; i < 7; i++) {
+    for (j = 0; j < numbers[i]; j++) {
+      result.push(letters[i]);
+    }
+  }
+}
+
+function mainProgram(value) {
+  devideNumber(value);
+  console.log(result.join(''));
 
   return;
 }
 
-function getU8Code(valueRemains) {
-
-}
+mainProgram(value);
