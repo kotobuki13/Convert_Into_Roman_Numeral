dd89e5acbde1bb963e69273a75f2b3ece634e987
kotobuki13 <kotobukigit@yahoo.co.jp>
2020-01-01 18:37:29 -0800

    値を切り捨てするようの変更

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-const value = 130; // get value from user
+let value = 8; // get value from user
 const result = []
 const letters = ["m", "d", "c", "l", "x", "v", "i"];
 
+const target = document.getElementById("target");
+
 function devideNumber(value) {
 
-  let numbers = new Array(7); // m, d, c, l, x, v, remain
+  let numbers = [0, 0, 0, 0, 0, 0, 0]; // m, d, c, l, x, v, remain
 
   if (value / 1000 >= 1) {
-    numbers[0] = value / 1000;
+    numbers[0] = Math.floor(value / 1000);
     value -= numbers[0] * 1000;
   }
   if (value / 500 >= 1) {
-    numbers[1] += 1;
+    numbers[1] = Math.floor(value / 500);
+    value -= numbers[1] * 500;
   }
   if (value / 100 >= 1) {
-    numbers[2] = value / 100;
+    numbers[2] = Math.floor(value / 100);
     value -= numbers[2] * 100;
   }
   if (value / 50 >= 1) {
-    numbers[3] += 1;
+    numbers[3] = Math.floor(value / 50);
+    value -= numbers[3] * 50;
   }
   if (value / 10 >= 1) {
-    numbers[4] = value / 10;
+    numbers[4] = Math.floor(value / 10);
     value -= numbers[4] * 10;
   }
   if (value / 5 >= 1) {
-    numbers[5] += 1;
+    numbers[5] = Math.floor(value / 5);
+    value -= numbers[5] * 5;
   }
   if (value > 0) {
     numbers[6] = value;
@@ -45,7 +50,8 @@ function writeRomanNum(numbers) {
 }
 
 function printResult() {
-  console.log(result.join(''));
+  resultOnHTML = result.join('');
+  target.textContent = resultOnHTML;
 
   return;
 }
